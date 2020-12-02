4bb0a8c84c7c8bf6f9579a31c4e53247dac33375
Samantha Zeitlin <samanthazeitlin@gmail.com>
2020-12-02 12:00:26 -0800

    part 2 of day 1

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import itertools
 
 def report_repair(entries:List[int]) -> Tuple[int, int]:
     """
@@ -21,8 +22,20 @@ def report_repair(entries:List[int]) -> Tuple[int, int]:
                 i = i + 1
                 total = 2020
 
+
+def report_repair_trio(entries: List[int]) -> Tuple[int, int, int]:
+    """
+    Find the trio of numbers that sums to 2020, and then multiply them
+
+    :param entries: this is a long list of integers
+    :return: result of the multiplication 
+    """
+    for trio in itertools.combinations(entries, 3):
+        if sum(trio) == 2020:
+            return trio
+
 if __name__ == '__main__':
     with open('entries.txt', 'r') as f:
         entries = [int(x) for x in f.readlines()]
-        (one,two) = report_repair(entries)
-        print(one * two)
+        (one,two, three) = report_repair_trio(entries)
+        print(one * two * three)
--- test_day1.py
+++ test_day1.py
@@ -1,5 +1,5 @@
 import unittest
-from day1 import report_repair
+from day1 import report_repair, report_repair_trio
 
 class TestSimpleCase(unittest.TestCase):
 
@@ -7,6 +7,13 @@ class TestSimpleCase(unittest.TestCase):
         ls = [1721,979,366,299,675,1456]
         (one, two) = report_repair(ls)
         assert (one, two) ==  (1721, 299)
+
+class TestHarderCase(unittest.TestCase):
+
+    def test_short_list(self):
+        ls = [1721, 979, 366, 299, 675, 1456]
+        (one, two, three) = report_repair_trio(ls)
+        assert (one, two, three) == (979, 366, 675)
 
 if __name__ == '__main__':
     unittest.main()
