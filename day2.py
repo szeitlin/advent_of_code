261fbd9bb0a1afbc477b6162e0193b090e823e08
Samantha Zeitlin <samanthazeitlin@gmail.com>
2020-12-06 15:23:22 -0800

    part 2 of day 2

--- day2.py
+++ day2.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import List
+from typing import List, Tuple
 
 
 def parse_input(day2_entries:List[str]) -> int:
@@ -10,8 +10,6 @@ def parse_input(day2_entries:List[str]) -> int:
     The letter 'a' should appear at least 1 time and not more than 3 times
     in the password abcde
     
-    :param list: 
-    :return: 
     """
     split_map = [x.split(': ') for x in day2_entries]
     rules = [x[0] for x in split_map]
@@ -32,7 +30,41 @@ def count_letter_in_password(password:str, letter:str) -> int:
     return counts[letter]
 
 
+def part2(day2_entries:List[str]) -> int:
+    """
+    More like what I thought it was originally
+    
+    Two positions are listed; the letter must be in exactly one (only one!) of those
+    
+    Example: 
+    1-3 a: abcde 
+    valid because it has 'a' at position 1 and not at position 3
+    
+    """
+    split_map = [x.split(': ') for x in day2_entries]
+    rules = [x[0] for x in split_map]
+    times = [x.split()[0].split('-') for x in rules]
+    locs = [(int(x[0]), int(x[1])) for x in times]
+    letters = [x.split()[1] for x in rules]
+    passwords = [x[1] for x in split_map]
+    result = 0
+    for password,letter,loc in zip(passwords,letters,locs):
+        result += check_letter_locs(password,letter,loc)
+    return result
 
+def check_letter_locs(password:str, letter:str, loc:Tuple[int,int]) -> int:
+    """
+    Helper function
+    
+    :param password: look in this string
+    :param letter: for this letter
+    :param loc: at these locations
+    :return: 1 if valid, 0 if not
+    """
+    if (password[loc[0]-1]==letter) ^ (password[loc[1]-1]==letter):
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
@@ -40,3 +72,5 @@ if __name__ == '__main__':
         day2_entries = f.readlines()
         result = parse_input(day2_entries)
         print(result)
+        result2 = part2(day2_entries)
+        print(result2)
--- test_day2.py
+++ test_day2.py
@@ -1,14 +1,32 @@
 import unittest
-from day2 import parse_input
+from day2 import parse_input, part2
 
 class TestParseInput(unittest.TestCase):
 
     def test_simple(self):
-        day2_entries = ["1-3 a: abcde",
-                        "1-3 b: cdefg",
-                        "2-9 c: ccccccccc"]
+        day2_entries = ["1-3 a: abcde", #valid
+                        "1-3 b: cdefg", #invalid
+                        "2-9 c: ccccccccc"] #valid
         result = parse_input(day2_entries)
         assert result == 2
+
+class TestPart2(unittest.TestCase):
+
+    def test_simple(self):
+        day2_entries = ["1-3 a: abcde", #valid
+                        "1-3 b: cdefg", #example of neither
+                        "2-9 c: ccccccccc"] #example of both
+        result = part2(day2_entries)
+        assert result == 1
+
+    def test_longer(self):
+        examples = ["5-10 b: bhbjlkbbbbbbb", #valid
+                    "3-4 j: hjvj", #valid
+                    "8-9 p: pmljtsttp", #valid
+                    "3-4 t: hvtttqhdjmmnbqwbgfs", #invalid (both)
+                    "4-6 m: mblwtzmvmdjkkmmtsckm"] #invalid (neither)
+        result = part2(examples)
+        assert result == 3
 
 if __name__ == '__main__':
     unittest.main()
