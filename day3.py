dd9361272bfb268aa3984026ec6bee25adbfb889
Samantha Zeitlin <samanthazeitlin@gmail.com>
2020-12-11 12:00:41 -0800

    This still gets the wrong answer on the site, but the tests pass

--- day3.py
+++ day3.py
@@ -1,21 +1,3 @@
-import math
-
-def prep_input(inputmatrix:list) -> list:
-    """
-    Seems like they want you to replicate the input several fold,
-    since the initial input they give is very narrow, 
-    but then the examples they show are wider
-    :param inputmatrix: narrow input
-    :return: wider input
-    """
-    #how wide to make it? it's a triangle
-    length = len(inputmatrix) #number of rows
-    #y = mx + b where b is 0, solve for x
-    replicates = int(length//3) #this has to be an int
-    wider = [replicates * x for x in inputmatrix]
-    return wider
-
-
 
 def count_trees(lines) -> int:
     """
@@ -30,11 +12,12 @@ def count_trees(lines) -> int:
     """
     trees = 0
     right = 0
-    for i, x in enumerate(lines):
-        i += 1  # down 1
+    width = len(lines[0]) #length of one line
+    for x in lines[1:]:
         right += 3  # over 3
+        right = right % width #wrap around
         try:
-            if lines[i][right] == '#':
+            if x[right] == '#':
                 trees += 1
         except IndexError as e:
             print('hit index error')
@@ -45,5 +28,4 @@ if __name__ == '__main__':
 
     with open('day3_input.txt', 'r') as f:
         inputmatrix = f.readlines()
-    lines = prep_input(inputmatrix)
-    trees = count_trees(lines)
+    trees = count_trees(inputmatrix)
