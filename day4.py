60b51a6e6946a603c1d6ba88b9dc98af594c2f5d
Samantha Zeitlin <samanthazeitlin@gmail.com>
2020-12-11 14:12:16 -0800

    WIP: part 2 of day 4 is really tedious

--- day4.py
+++ day4.py
@@ -10,15 +10,43 @@ def read_batch_files(inputdoc:str) -> list:
 def validate_passport_data(document:str) -> int:
     """
     Convert unstructured text to dictionaries and 
-    check for missing fields
     
-    cid is optional, all others are required
     
-    :return: valid as 1 or invalid as 0
+    :return: 
+    """
+    groups = find_fields(document)
+    if check_fields_present(groups) == 1:
+        #check if they're actually valid
+        #byr: (v >= 1920) and (v <= 2002)
+        #iyr: (v >= 2010) and (v <= 2020)
+        #eyr: (v >= 2020) and (v <= 2030)
+        #hgt: if cm (v >= 150) and (v <= 193)
+              #if in (v >= 59) and (v <= 76)
+        #hcl: v.startswith('#') and [0-9]{6} or [a-f]{6}
+        #ecl: v in 'amb blu brn gry grn hzl oth'
+        #pid: len(v) == 9
+        
+def find_fields(document:str) -> list:
+    """
+    Find which fields are present
+    
+    :param document: string with fields
+    :return: list of fields found
     """
     cleaned = document.replace(": ", ":")
     m = re.compile('(\w*:.*?)')
     groups = re.findall(m, cleaned)
+    return groups
+
+def check_fields_present(groups:list) -> int:
+    """
+    check for missing fields
+        
+    cid is optional, all others are required
+    
+    :param groups: keys
+    :return: valid as 1 or invalid as 0
+    """
     required = {'ecl:', 'pid:', 'eyr:', 'hcl:', 'byr:', 'iyr:', 'cid:', 'hgt:'}
     missing = required - set(groups)
     if len(missing) == 0:
@@ -47,4 +75,3 @@ if __name__ == '__main__':
         inputdoc = f.read()
     valid = count_valid(inputdoc)
     print(valid)
-    
