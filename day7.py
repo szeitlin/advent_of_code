6e158f8e34eb9e22e9fc25a19671cd63444c48da
Samantha Zeitlin <samanthazeitlin@gmail.com>
2020-12-21 09:46:08 -0800

    WIP

--- day7.py
+++ day7.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 def rule_parser(rule:str):
     """
@@ -9,5 +9,61 @@ def rule_parser(rule:str):
     """
     parts = rule.split(' bags contain ')
     container = parts[0]
-    contained = parts[1].split(', ')
+    try:
+        contained = parts[1].split(', ')
+    except IndexError:
+        contained = parts[1]
     return {container:contained}
+
+def all_rules(rule_list:List[str]) -> dict:
+    """
+    Parse all the lines in the rule list into a single dict. 
+    :param rule_list: list with one set of rules per line
+    :return: dictionary of {color: [what it contains]}
+    """
+    big_dict = dict()
+    for rule in rule_list:
+        big_dict.update(rule_parser(rule))
+    return big_dict
+
+def find_target_holders(big_dict:dict, target:str='shiny gold bag') -> List:
+    """
+    You have a shiny gold bag. 
+    If you wanted to carry it in at least one other bag, 
+    how many different bag colors would be valid for the outermost bag? 
+    (In other words: how many colors can, eventually, contain at least one shiny gold bag?)
+    
+    :param big_dict: 
+    :param target bag type
+    :return: set of bags
+    """
+    holders = []
+    for k,v in big_dict.items():
+        for item in v:
+            if target in item:
+                holders.append(k)
+    #print(holders)
+    unique = set(holders)
+    return list(unique)
+
+def find_holder_holders(big_dict:dict) -> List:
+    """
+    Trace upwards through the graph to find all the bags that can contain shiny gold ones
+    :param big_dict: 
+    :return: list of bags
+    """
+    #first, find any bags that can contain shiny gold (using find_shiny_gold_holders)
+    holders = find_target_holders(big_dict)
+
+    #iterate back through the big_dict and find what bags contain those
+    #again, using find_target_holders()
+    all_holders = holders[:]
+    for k in holders:
+        for item in k:
+        #todo: what is the stopping criteria if we did this with a while loop or recursion?
+            all_holders.extend(find_target_holders(big_dict=big_dict, target=item))
+
+    unique = set(all_holders)
+    return list(unique)
+
+
--- test_day7.py
+++ test_day7.py
@@ -1,5 +1,5 @@
 import unittest
-from day7 import rule_parser
+from day7 import rule_parser, all_rules, find_target_holders, find_holder_holders
 
 class TestRuleParser(unittest.TestCase):
 
@@ -16,6 +16,51 @@ class TestRuleParser(unittest.TestCase):
         test4 = "faded blue bags contain no other bags."
         parsed4 = rule_parser(test4)
         assert parsed4 == {'faded blue': ['no other bags.']}
+
+    def test_all_rules(self):
+        rules = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags."""
+        raw_list = [x.lstrip() for x in rules.split('.')]
+        rule_list = [x for x in raw_list if x != '']
+        assert rule_list[0] == "light red bags contain 1 bright white bag, 2 muted yellow bags"
+        assert rule_list[1] == "dark orange bags contain 3 bright white bags, 4 muted yellow bags"
+        big_dict = all_rules(rule_list)
+        assert len(big_dict.keys()) == 9
+        print(big_dict)
+
+class TestFindShinyGold(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rules = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags."""
+        raw_list = [x.lstrip() for x in rules.split('.')]
+        rule_list = [x for x in raw_list if x != '']
+        cls.big_dict = all_rules(rule_list)
+
+    def test_find_shiny_gold_holders(self):
+        holders = find_target_holders(big_dict=self.big_dict)
+        assert len(holders) == 2
+        assert set(holders) == {'bright white', 'muted yellow'}
+
+    def test_find_holder_holders(self):
+        holders = find_holder_holders(big_dict = self.big_dict)
+        print(holders)
+        assert len(holders) == 4
 
 if __name__ == '__main__':
     unittest.main()
