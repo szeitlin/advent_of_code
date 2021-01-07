ee2bb5510cd2f7e5de5ec87f1a67492d54bfa83d
Samantha Zeitlin <samanthazeitlin@gmail.com>
2021-01-07 14:43:52 -0800

    second test passing

--- test_day9.py
+++ test_day9.py
@@ -8,5 +8,31 @@ class TestSumPrevious(unittest.TestCase):
         result = sum_previous(xmaslist)
         assert result == 100
 
+    def test_example(self):
+        raw = """35
+        20
+        15
+        25
+        47
+        40
+        62
+        55
+        65
+        95
+        102
+        117
+        150
+        182
+        127
+        219
+        299
+        277
+        309
+        576"""
+        xmaslist = [int(x) for x in raw.split()]
+        print(xmaslist)
+        result = sum_previous(xmaslist, n=5)
+        assert result == 127
+
 if __name__ == '__main__':
     unittest.main()
