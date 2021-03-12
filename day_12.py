4df558ec056c1ffc5d910b3105a3bd1a86565ab6
Samantha Zeitlin <samanthazeitlin@gmail.com>
2021-03-12 15:00:34 -0800

    WIP basic instructions

--- day_12.py
+++ day_12.py
@@ -1,40 +1,59 @@
 import operator
 import re
-from typing import Tuple
 
-def move_ship(start:Tuple[int, int], facing:str, instr:str) -> Tuple[int, int]:
+def move_ship(start:list, instr:str) -> list:
     """
     Assuming a coordinate system of (-inf, inf) on the x and (-inf, inf) on the y
     
     :param start: wherever the ship is now, before executing a step
-    :param facing: whichever way the ship is facing now, before executing a step
     :param instr: looks like NSEW, LR, or F (forward), plus a number
-    Only LR (left and right) can change the direction the ship is facing
+
     
     :return: new coordinates
     """
     #Let's assume East is + and West is -
     #Let's also assume North is + and South is -
 
-    #todo: probably have to use a regex here
     step_dir = re.match('^(\D)', instr).group()
     step_size = int(re.search('(\d+)', instr).group())
 
     direction_map = {'N': operator.add(start[1], step_size),
                      'S': operator.sub(start[1], step_size),
                       'E': operator.add(start[0], step_size),
-                        'W': operator.sub(start[0], step_size),
-                        'L':,
-                        'R':}
+                        'W': operator.sub(start[0], step_size)}
 
-#todo: write functions for left and right rotations
+    step = direction_map[step_dir]
+    print(step)
 
-def manhattan_distance(start:Tuple[int,int], end:Tuple[int,int]):
+    result = start
+
+    if step_dir in 'NS':
+        print('North or South')
+        result = [start[0], step]
+        print(result)
+
+    elif step_dir in 'EW':
+        print('East or West')
+        result = [step, start[1]]
+        print(result)
+
+    return result
+
+#todo: write functions for handling facing, left and right rotations
+#Only LR (left and right) can change the direction the ship is facing
+
+
+def manhattan_distance(start:list, end:list) -> int:
     """
     sum of the absolute values of east/west and north/south position
     
-    :param start: 
-    :param end: 
-    :return: 
+    abs(x2-x1) + abs(y2-y1)
+    
+    :param start: starting location
+    :param end: final location, after executing all steps in the sequence
+    :return: distance
     """
+    y = operator.abs(end[1] - start[1])
+    x = operator.abs(end[0] - start[0])
+    return x + y
 
--- test_day12.py
+++ test_day12.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+from day_12 import manhattan_distance, move_ship
 
 class TestInstructions(unittest.TestCase):
 
@@ -9,6 +10,26 @@ class TestInstructions(unittest.TestCase):
         step_size = int(re.search('(\d+)', instr).group())
         assert step_dir == 'F'
         assert step_size == 10
+
+class TestMoveShip(unittest.TestCase):
+
+    def test_move_ship_north(self):
+        start = [0,0]
+        instr= 'N3'
+        moved = move_ship(start, instr)
+        assert moved == [0,3]
+
+    def test_move_ship_west(self):
+        start = [0,0]
+        instr='W11'
+        moved = move_ship(start, instr)
+        assert moved == [-11, 0]
+        
+class TestManhattanDistance(unittest.TestCase):
+
+    def test_manhattan_distance(self):
+        self.fail()
+
 
 if __name__ == '__main__':
     unittest.main()
